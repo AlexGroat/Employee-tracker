6447b968701010124f2239517093adf63560476f
Alex Groat <alex.groat.95@gmail.com>
2021-11-23 13:13:02 +1030

    start questions

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const table = require('console.table');
 
-// create connection to sql database
-
-const connect = mysql.createConnection({
+// create connection (coding standard to name connection) to sql database
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3301,
     user: 'root',
@@ -13,8 +12,14 @@ const connect = mysql.createConnection({
     database: 'employees_db'
 })
 
+// connect to server and db
+connection.connect(function (err) {
+    if (err) throw err;
+    selectMenu();
+})
+
 var figlet = require('figlet');
-figlet('Employee \n Management \n System', function(err, data) {
+figlet('Employee \n Management \n System', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
@@ -23,9 +28,23 @@ figlet('Employee \n Management \n System', function(err, data) {
     console.log(data)
 });
 
+function selectMenu() {
+    inquirer
+        .prompt({
+            name: 'choice',
+            type: 'list',
+            message: 'Welcome to the employee database! Please select an option from the menu below.', 
+            choices: [
+                'View all departments',
+            ]
+        })
+}
+
+
+
 // create inquirer prompts with choices
 
-/* 
+/*
 view all departments
 view all roles
 view all employees
