dbb8f194cf291b9ec3b7e18a35c916c95483211d
Alex Groat <alex.groat.95@gmail.com>
2021-11-27 14:46:02 +1030

    updated with comments on left to do

--- server.js
+++ server.js
@@ -122,10 +122,6 @@ function addDepartment() {
 // create inquirer prompts with choices
 
 /*
-view all departments
-view all roles
-view all employees
-add a department
 add a role
 add an employee
 update role
